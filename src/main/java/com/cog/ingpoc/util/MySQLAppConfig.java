e974b9b392712be6489101f1ed9cc7a8a2429d66
ingpoc <gupta.rp.gurusharan@gmail.com>
2018-09-11 14:25:04 +0530

    Changes for working application
    Changes for working application

--- src/main/java/com/cog/ingpoc/util/MySQLAppConfig.java
+++ src/main/java/com/cog/ingpoc/util/MySQLAppConfig.java
@@ -5,7 +5,7 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
